67334dbc0e22685d2b174003b4d62b1740df9543
gbrlbrbs <gabriel.barbosa.martinz@outlook.com>
2025-02-24 23:51:10 -0300

    retraining yolo

--- src/cv_cli/__init__.py
+++ src/cv_cli/__init__.py
@@ -1,9 +1,24 @@
 import argparse
 from .video_proc import process_video
 
+
 def main() -> None:
-    ap = argparse.ArgumentParser(description="Extract license plate information and prints to terminal")
-    ap.add_argument("video_path", help="path to the video file")
-    ap.add_argument("-m", "--model", help="Path to retrained model, if blank it defaults to ./runs/detect/train/best.pt", default="./runs/detect/train/best.pt")
+    ap = argparse.ArgumentParser(
+        description="Extract license plate information and prints to terminal"
+    )
+    ap.add_argument("videopath", help="path to the video file")
+    ap.add_argument(
+        "-m",
+        "--model",
+        help="Path to retrained model, if blank it defaults to ./runs/detect/train/weights/best.pt",
+        default="./runs/detect/train6/weights/best.pt",
+    )
+    ap.add_argument(
+        "-f",
+        "--frame-skip",
+        help="Process every frame-skip frames. Defaults to 4.",
+        default=4,
+        type=int,
+    )
     args = ap.parse_args()
-    process_video(args.video_path, args.model)
+    process_video(args.videopath, args.model, args.frame_skip)
--- src/cv_cli/video_proc.py
+++ src/cv_cli/video_proc.py
@@ -2,30 +2,56 @@ import cv2
 from pathlib import Path
 from ultralytics import YOLO
 from ultralytics.engine.results import Results
-# import time
+from dataclasses import dataclass
 
 VEHICLE_CLASS_IDS = (2, 3, 5, 7)
 
 
-def detect_license_plates(frame: cv2.Mat, detections: Results, model_file: Path):
-    
+@dataclass
+class LicensePlate:
+    frame: cv2.Mat
+    track_id: int
+    score: float
+
+    def save(self, filename: str | None = None):
+        if self.frame.size != 0:
+            if filename:
+                cv2.imwrite(f"{filename}.jpg", self.frame)
+            else:
+                cv2.imwrite(f"{self.track_id}.jpg", self.frame)
+        else:
+            print('No frame to save!')
+
+
+def detect_license_plates(
+    frame: cv2.Mat, detections: Results, model_file: Path
+) -> list[LicensePlate]:
+    license_plate_objs = []
     license_plate_detection = YOLO(model_file)
     for det in detections.boxes.data.tolist():
         x1, y1, x2, y2, track_id, score, class_id = det
         if int(class_id) in VEHICLE_CLASS_IDS and score > 0.5:
-            roi = frame[int(y1):int(y2), int(x1):int(x2)]
+            roi = frame[int(y1) : int(y2), int(x1) : int(x2)]
             license_plates: Results = license_plate_detection(roi)[0]
             for plate in license_plates.boxes.data.tolist():
                 px1, px2, py1, py2, pscore, _ = plate
-                print(f"license plate at bounding box ({py1}, {px1}) ({py2}, {px2}), track_id={track_id}")
+                plate_frame = roi[int(py1) : int(py2), int(px1) : int(px2)]
+                lp = LicensePlate(
+                    frame=plate_frame, track_id=int(track_id), score=pscore
+                )
+                license_plate_objs.append(lp)
+                print(
+                    f"license plate at bounding box ({py1}, {px1}) ({py2}, {px2}), track_id={track_id}"
+                )
+    return license_plate_objs
 
 
-def process_video(path: str, model_path: str, frame_skip=4):
+def process_video(path: str, model_path: str, frame_skip: int):
     """Process the video file
 
     Args:
         path (str): Path of the video file
-        frame_skip (int, optional): Process every {frame_skip} frames. Defaults to 4.
+        frame_skip (int, optional): Process every {frame_skip} frames.
 
     Raises:
         ValueError: Whenever the file doesn't exist
@@ -34,11 +60,11 @@ def process_video(path: str, model_path: str, frame_skip=4):
     file = Path(path).resolve()
     if not file.exists():
         raise ValueError(f"Path doesn't exists: {file}")
-    
+
     cap = cv2.VideoCapture(str(file))
     if not cap.isOpened():
         raise RuntimeError("Could not open video capture")
-    
+
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = cap.get(cv2.CAP_PROP_FPS)
     duration = frame_count / fps
@@ -47,14 +73,17 @@ def process_video(path: str, model_path: str, frame_skip=4):
 
     frame_n = 0
     model_file = Path(model_path).resolve()
-    car_detection = YOLO('yolov8s.pt')
+    car_detection = YOLO("yolov8s.pt")
 
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
-        
         if frame_n % frame_skip == 0:
             detections = car_detection.track(frame, persist=True)[0]
-            detect_license_plates(frame, detections, model_file)
+            license_plates = detect_license_plates(frame, detections, model_file)
+            for plate in license_plates:
+                plate.save()
+        frame_n += 1
 
+    cap.release()

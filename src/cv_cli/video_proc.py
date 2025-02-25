0b9fe763474a2e055facd8a804b15e1f0818a072
gbrlbrbs <gabriel.barbosa.martinz@outlook.com>
2025-02-24 21:01:12 -0300

    process and print bounding box

--- src/cv_cli/video_proc.py
+++ src/cv_cli/video_proc.py
@@ -1,17 +1,26 @@
 import cv2
 from pathlib import Path
+from ultralytics import YOLO
+from ultralytics.engine.results import Results
 # import time
 
+VEHICLE_CLASS_IDS = (2, 3, 5, 7)
 
-def preprocess_frame(f: cv2.Mat):
-    gray = cv2.cvtColor(f, cv2.COLOR_BGR2GRAY)
-    filtered = cv2.bilateralFilter(f, 2, 100, 100)
 
-    # edges
-    edged = cv2.Canny(filtered, 30, 80)
-    return gray, edged
+def detect_license_plates(frame: cv2.Mat, detections: Results, model_file: Path):
+    
+    license_plate_detection = YOLO(model_file)
+    for det in detections.boxes.data.tolist():
+        x1, y1, x2, y2, track_id, score, class_id = det
+        if int(class_id) in VEHICLE_CLASS_IDS and score > 0.5:
+            roi = frame[int(y1):int(y2), int(x1):int(x2)]
+            license_plates: Results = license_plate_detection(roi)[0]
+            for plate in license_plates.boxes.data.tolist():
+                px1, px2, py1, py2, pscore, _ = plate
+                print(f"license plate at bounding box ({py1}, {px1}) ({py2}, {px2}), track_id={track_id}")
 
-def process_video(path: str, frame_skip=4):
+
+def process_video(path: str, model_path: str, frame_skip=4):
     """Process the video file
 
     Args:
@@ -24,7 +33,7 @@ def process_video(path: str, frame_skip=4):
     """
     file = Path(path).resolve()
     if not file.exists():
-        raise ValueError(f"Path doesn't exists: {path}")
+        raise ValueError(f"Path doesn't exists: {file}")
     
     cap = cv2.VideoCapture(str(file))
     if not cap.isOpened():
@@ -36,9 +45,9 @@ def process_video(path: str, frame_skip=4):
 
     print(f"Duration: {duration:.2f} seconds; {frame_count} frames")
 
-    # detected_plates = set()
     frame_n = 0
-    # start = time.time()
+    model_file = Path(model_path).resolve()
+    car_detection = YOLO('yolov8s.pt')
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -46,12 +55,6 @@ def process_video(path: str, frame_skip=4):
             break
         
         if frame_n % frame_skip == 0:
-            gray, edged = preprocess_frame(frame)
-            # cv2.imshow("Grayscale", gray)
-            resize_to_show = cv2.resize(edged, (1920, 1080))
-            cv2.imshow("Edges", resize_to_show)
-
-            if cv2.waitKey(1) == ord('q'):
-                break
-
+            detections = car_detection.track(frame, persist=True)[0]
+            detect_license_plates(frame, detections, model_file)
 

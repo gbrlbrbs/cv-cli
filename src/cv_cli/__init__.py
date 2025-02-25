e6a5d9cef3341e1a3f4120c26de253ea7ab819ad
gbrlbrbs <gabriel.barbosa.martinz@outlook.com>
2025-02-24 21:00:56 -0300

    add model file

--- src/cv_cli/__init__.py
+++ src/cv_cli/__init__.py
@@ -4,5 +4,6 @@ from .video_proc import process_video
 def main() -> None:
     ap = argparse.ArgumentParser(description="Extract license plate information and prints to terminal")
     ap.add_argument("video_path", help="path to the video file")
+    ap.add_argument("-m", "--model", help="Path to retrained model, if blank it defaults to ./runs/detect/train/best.pt", default="./runs/detect/train/best.pt")
     args = ap.parse_args()
-    process_video(args.video_path)
+    process_video(args.video_path, args.model)
